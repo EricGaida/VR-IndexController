1d24350bbcaed4aaad19bf4c2bb33a8abf47c14c
RapidDuck <ericgaida1@gmail.com>
2019-11-08 12:27:52 +0100

    deleted obsolete code

--- IndexController/Assets/Prefabs/InteractableRocket/RocketInteractable.cs
+++ IndexController/Assets/Prefabs/InteractableRocket/RocketInteractable.cs
@@ -4,19 +4,10 @@ using UnityEngine;
 using Valve.VR;
 public class RocketInteractable : MyInteractable {
 
-    public GameObject flyingLocomotion;
-
-    public override void Action() {
-        // enables flying locomotion
-        flyingLocomotion.GetComponent<FlyLocomotion>().enabled = true;
-    }
-
     public override void OnPickup(MyHand hand) {
         base.OnPickup(hand);
         // hide picked up item
         GetComponent<MeshRenderer>().enabled = false;
-        // enables flying locomotion
-        flyingLocomotion.GetComponent<FlyLocomotion>().enabled = true;
         // enables rocket hand model
         hand.transform.GetChild(2).gameObject.SetActive(true);
     }
@@ -24,8 +15,6 @@ public class RocketInteractable : MyInteractable {
     public override void OnDrop(MyHand hand) {
         // enables picked up item
         GetComponent<MeshRenderer>().enabled = true;
-        // disables flying locomotion
-        flyingLocomotion.GetComponent<FlyLocomotion>().enabled = false;
         // disables rocket hand model
         hand.transform.GetChild(2).gameObject.SetActive(false);
         base.OnDrop(hand);
